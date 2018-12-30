4cc3861985c4a6829bd0c7fb55bf5e9de9a07aa8
Matt Duffield <matt.duffield@gmail.com>
2018-12-30 15:53:47 -0800

    Auto commit from FrontEnd Creator

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ const config = {
 const game = new Phaser.Game(config);
 let graphics;
 let path
+let ENEMY_SPEED = 1/10000;
 
 function preload() {
   // load the game assets - enemy and turret atlas
@@ -35,16 +36,55 @@ function create() {
   graphics.lineStyle(3, 0xffffff, 1);
   // visualize the path
   path.draw(graphics);
+  enemies = this.add.group({ classType: Enemy, runChildUpdate: true });
+	this.nextEnemy = 0;  
 }
 function update() {
+  // if its time for the next enemy
+  if (time > this.nextEnemy) {
+    var enemy = enemies.get();
+    if (enemy) {
+      enemy.setActive(true);
+      enemy.setVisible(true);
 
+      // place the enemy at the start of the path
+      enemy.startOnPath();
+
+      this.nextEnemy = time + 2000;
+    }
+  }
 }
 
 let Enemy = new Phaser.Class({
   Extends: Phaser.GameObjects.Image,
   initialize: function Enemy (scene) {
     Phaser.GameObjects.Image.call(this, scene, 0, 0, 'sprites', 'enemy');
+    this.follower = { t: 0, vec: new Phaser.Math.Vector2() };
   },
   update: function (time, delta) {
-  }
+    // move the t point along the path, 0 is the start and 0 is the end
+    this.follower.t += ENEMY_SPEED * delta;
+    
+    // get the new x and y coordinates in vec
+    path.getPoint(this.follower.t, this.follower.vec);
+    
+    // update enemy x and y to the newly obtained x and y
+    this.setPosition(this.follower.vec.x, this.follower.vec.y);
+
+    // if we have reached the end of the path, remove the enemy
+    if (this.follower.t >= 1) {
+      this.setActive(false);
+      this.setVisible(false);
+    }
+  },
+  startOnPath: function () {
+    // set the t parameter at the start of the path
+    this.follower.t = 0;
+    
+    // get x and y of the given t point            
+    path.getPoint(this.follower.t, this.follower.vec);
+    
+    // set the x and y of our enemy to the received from the previous step
+    this.setPosition(this.follower.vec.x, this.follower.vec.y);
+  },
 });
